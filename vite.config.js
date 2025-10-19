ed30f04c119167fcc2d5cca5a3532e59c98b43b7
Jcsumugat <jcsumugatxd@gmail.com>
2025-10-19 13:13:01 +0800

    Final fix for Railway deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: process.env.ASSET_URL || '/',
     plugins: [
         laravel({
             input: [
@@ -16,10 +17,6 @@ export default defineConfig({
     build: {
         manifest: true,
         outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            },
-        },
+        emptyOutDir: true,
     },
 });
